237fbb1294cd35214050faf554e585311d1bd622
Adityakbr01 <aditykbr01@gmail.com>
2024-10-16 23:27:05 +0530

    ""

--- services/imageService.ts
+++ services/imageService.ts
@@ -1,6 +1,8 @@
 export const getUserImageSrc = (imagePath: string | undefined) => {
   if (imagePath) {
-    return { uri: imagePath };
+    return {
+      uri: `https://jpeecghjhpmpupbhijgu.supabase.co/storage/v1/object/public/Uploads/profile/${imagePath}`,
+    };
   } else {
     return require("../assets/images/defaultUser.png");
   }
